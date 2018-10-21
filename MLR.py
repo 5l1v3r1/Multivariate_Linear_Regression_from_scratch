1d8ce9ed573b893d10441453fb4304a1c6925d1c
Amit Singh <amit8564880158@gmail.com>
2018-10-21 22:35:41 +0530

    Update MLR.py

--- MLR.py
+++ MLR.py
@@ -11,27 +11,31 @@ import matplotlib.pyplot as plt
 
 np.set_printoptions(suppress=True)
 
-data=pd.read_csv("abc.txt",names=["size","bedroom","price"])
-data.dropna()
-data=(data-data.mean())/(data.std())
-x=np.array(data["size"]).reshape(-1,1)
-x1=np.array(data["bedroom"]).reshape(-1,1)
-y=np.array(data["price"]).reshape(-1,1)
-one=np.ones((x.shape[0])).reshape(-1,1)
-x=np.hstack((one,x,x1))
 
-#theta=np.random.randn(3,1)*0.001
-theta=np.array([1,2,3])
-alpha=0.001
+x,y,alpha,theta,data,one=None,None,None,None,None,None
+
+def data_preprocess():
+    global x,y,one,data,theta,alpha
+    data=pd.read_csv("abc.txt",names=["size","bedroom","price"])
+    data.dropna()
+    data=(data-data.mean())/(data.std())
+    x=np.array(data.iloc[:,0:2])
+    y=np.array(data["price"])
+    theta=np.random.randn(3,1)
+    alpha=0.01
+    y=y.reshape(-1,1)
+    one=np.ones((x.shape[0])).reshape(-1,1)
+    x=np.hstack((one,x))
+
 
 def gradient(x,y):
     global theta
-    iter=10000
+    iter=1000
     for i in range(iter):
         yhat=np.matmul(x,theta)
         theta=theta-(alpha/len(x))*(np.matmul(x.T,(yhat-y)))
         c2=cost(x,y)
-        if(iter%1000==0):
+        if(iter%100==0):
             print(c2)
     return theta
 
@@ -40,6 +44,7 @@ def cost(x,y):
     c1=(1/(2*len(x)))*(np.sum(np.power(np.subtract(yhat,y),2)))
     return c1
 
+data_preprocess()
 gradient(x,y)
 print(theta)
 
